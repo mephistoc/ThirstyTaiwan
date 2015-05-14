ceea0bab1e8bfd7dc41212d97f6cd501a3a86aef
Vincent Chu <vincent@Vincents-MacBook-Pro.local>
2015-05-14 09:40:28 +0800

    Initial Commit

---
+++ ThirstyTaiwan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThirstyTaiwan
+//
+//  Created by Vincent Chu on 5/14/15.
+//  Copyright (c) 2015 Vincent Chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThirstyTaiwan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThirstyTaiwan
+//
+//  Created by Vincent Chu on 5/14/15.
+//  Copyright (c) 2015 Vincent Chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
